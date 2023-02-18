4121a1c76283e5226de72db0d824922369d55c65
EnesBozdemir <76599627+EnesBozdemir@users.noreply.github.com>
2023-02-18 18:03:02 +0300

    profile-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.module.css';
+import ProfileList from './components/profile/ProfileList'
+
+function App() {
+  return (
+    <div>
+      <ProfileList/>
+    </div>
+  );
+}
+
+export default App;
